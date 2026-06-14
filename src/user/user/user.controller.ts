f0db984b8022eec6f66ead83b00ab3dfc631fd2f
Fadel Rahmadhan <carleonfarm@gmail.com>
2026-06-14 16:00:48 +0700

    add-cookie-parser

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Post, Get, Req } from '@nestjs/common';
-import type { Request } from 'express';
+import { Controller, Post, Get, Req, Query, Res, HttpCode, Header, Redirect, } from '@nestjs/common';
+import type { Response, Request, } from 'express';
+import type { HttpRedirectResponse } from '@nestjs/common';
+
 
 @Controller('/api/users')
 export class UserController {
 
-    @Get('/:id')
-    getById(@Req() request: Request): string {
-        return `GET ${request.params.id}`;
+    @Get('/set-cookie')
+    setCookie(@Query('name') name: string, @Res() response: Response) {
+        response.cookie('name', name);
+        response.status(200).send('succes set cookie');
+    }
+
+    @Get('/get-cookie')
+    getCookie(@Req() requet: Request): string {
+        return requet.cookies['name'];
+    }
+
+
+    @Get('/sample-response')
+    @Header('content-Type', 'application/json')
+    @HttpCode(200)
+    sampleResponse(): Record<string, string> {
+        return {
+            data: 'Hello Json'
+        };
+    }
+    @Get('/redirect')
+    @Redirect()
+    redirect(): HttpRedirectResponse {
+        return {
+            url: '/api/users/sample-response',
+            statusCode: 301,
+        }
+    }
+
+    @Get('/hello')
+    async sayHello(
+        @Query('first_name') firstName: string,
+        @Query('last_name') lastName: string,
+    ): Promise<string> {
+        return `Hello ${firstName} ${lastName} `;
     }
 
     @Post()
